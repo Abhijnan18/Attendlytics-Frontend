f6ecea95e377402b9fbdedf6aa3f0f17e05f50e5
Abhijnan S <114734468+Abhijnan18@users.noreply.github.com>
2025-03-01 11:46:39 +0530

    bla

--- Attendlytics/src/components/ui/skeleton.tsx
+++ Attendlytics/src/components/ui/skeleton.tsx
@@ -6,7 +6,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      className={cn("animate-pulse rounded-md bg-stone-900/10 dark:bg-stone-50/10", className)}
       {...props}
     />
   )
