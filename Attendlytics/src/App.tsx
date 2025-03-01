60ad2de696e5dc749b0d1255bd0ab8f9bc4cc230
Abhijnan S <114734468+Abhijnan18@users.noreply.github.com>
2025-03-01 11:34:10 +0530

    added dashboard

--- Attendlytics/src/App.tsx
+++ Attendlytics/src/App.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
+import DashboardPage from "./layout/Dashboard";
 import HomePage from "./pages/HomePage"; // Create this file to house your home/landing page content
 import LoginPage from "./pages/LoginPage"; // Login page for different roles
 
@@ -13,6 +14,9 @@ const App: React.FC = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="login/:role" element={<LoginPage />} />
+        </Route>
+        <Route path="/dashboard" element={<DashboardPage />}>
+
         </Route>
       </Routes>
 
